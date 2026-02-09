5bc4afb1d3b45d34c4f5395ac34ec5c90451df96
Aycsi <207523432+aycsi@users.noreply.github.com>
2026-02-09 01:31:59 +0100

    base interface

---
+++ engine/agent.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <cstdint>
+
+struct Message {
+    enum Type { Fill, CancelAck, Reject };
+    Type type;
+    uint64_t order_id;
+    double px;
+    uint64_t qty;
+    uint64_t ts;
+};
+
+class Agent {
+public:
+    virtual ~Agent() = default;
+    virtual void on_message(const Message& m) = 0;
+    virtual void step(uint64_t ct) = 0;
+    int get_id() const { return id; }
+    
+protected:
+    int id;
+    Agent(int i) : id(i) {}
+};

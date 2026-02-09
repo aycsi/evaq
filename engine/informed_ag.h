311d320bb2e36248367ecd8809104d72c4de1965
Aycsi <207523432+aycsi@users.noreply.github.com>
2026-02-09 20:20:26 +0100

    logic

---
+++ engine/informed_ag.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "agent.h"
+#include "exchange.h"
+#include <random>
+
+class InformedAg : public Agent {
+public:
+    InformedAg(int id, Exchange* ex, double alpha, uint64_t qty);
+    void on_message(const Message& m) override;
+    void step(uint64_t ct) override;
+    
+private:
+    Exchange* exch;
+    double alph;
+    uint64_t sz;
+    std::mt19937 gen;
+    int dir = 0;
+    uint64_t next_time = 0;
+};
